2ec8b220e330ba65c80c4444142249e29effd38b
Abdullah-Alharbi <w2.alharbi.me@gmail.com>
2019-04-08 01:37:37 +0300

    added new disk storage

--- config/filesystems.php
+++ config/filesystems.php
@@ -51,7 +51,7 @@ return [
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'url' => env('APP_URL') . '/storage',
             'visibility' => 'public',
         ],
 
@@ -67,6 +67,10 @@ return [
             'driver' => 'local',
             'root'   => public_path() . '/uploads',
         ],
+        'assets' => [
+            'driver' => 'local',
+            'root'   => public_path() . '/uploads',
+        ]
     ],
 
 ];
